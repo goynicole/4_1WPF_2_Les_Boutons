1b62d2a773975278a9a2c8399e2d8a0318bb2889
goy <goy.nicole018@gmail.com>
2019-04-09 12:44:11 +0200

    Découverte du WPF

---
+++ Les_Boutons/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Les_Boutons
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //en cliquant deux fois sur le bouton le private void s'affiche dans le cs
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Tu as cliqué juste pour voir ça xD ");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bravo!!!!!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+﻿# Découverte du WPF  
+> La **technologie WPF** (Windows Presention Foundation) permet de faire des **applications graphiques**.  ## Exercice 2  
+Faire un bouton dans un nouveau projet **Application WPF**. Ce bouton doit permettre de faire apparaitre un message de votre choix dans une nouvelle fenêtre *(comme une alert en JavaScript)*. 
